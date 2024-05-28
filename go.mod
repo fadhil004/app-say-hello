84169627267ac0993fb903e25411cfb698f1a537
fadhil004 <m.fadhil238@gmail.com>
2024-05-28 22:38:39 +0700

    adding dependency

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/fadhil004/app-say-hello
+
+go 1.22.2
+
+require github.com/fadhil004/go-say-hello v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello "github.com/fadhil004/go-say-hello"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello())
+}
